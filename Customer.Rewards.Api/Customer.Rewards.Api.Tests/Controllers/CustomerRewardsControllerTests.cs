179a767af4c92d6b03a598e1e7349479939953b5
vijay.kumar <vijay.kumar@neudesic.com>
2023-03-08 16:07:07 +0530

    added unut test cases

---
+++ Customer.Rewards.Api/Customer.Rewards.Api.Tests/Controllers/CustomerRewardsControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Customer.Rewards.Api.Controllers;
+using Customer.Rewards.Api.Managers.Interface;
+using Customer.Rewards.Api.Response;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Customer.Rewards.Api.Tests.Controllers
+{
+    public class CustomerRewardsControllerTests
+    {
+        private readonly MockRepository mockRepository;
+
+        private readonly Mock<ICustomerRewardsManager> mockCustomerRewardsManager;
+        private readonly Mock<ILogger<CustomerRewardsController>> mockLogger;
+
+        public CustomerRewardsControllerTests()
+        {
+            mockRepository = new MockRepository(MockBehavior.Strict);
+            mockCustomerRewardsManager = mockRepository.Create<ICustomerRewardsManager>();
+            mockLogger = mockRepository.Create<ILogger<CustomerRewardsController>>();
+        }
+
+        [Fact]
+        public void GetCustomerRewards_Returns_Rewards()
+        {
+            // Arrange
+            long customerId = 1;
+            mockCustomerRewardsManager.Setup(c => c.GetCustomerRewardsById(customerId)).Returns(
+                new RewardPointsResponse
+                {
+                    TotalRewardPoints = 100,
+                    MonthlyRewardPoints = new List<MonthlyRewardPointsResponse> { new MonthlyRewardPointsResponse { TotalRewardPoints = 100, MonthYear = "Jan, 2023" } }
+                }
+                );
+
+            var controller = new CustomerRewardsController(mockLogger.Object, mockCustomerRewardsManager.Object);
+
+            // Act
+            var result = controller.Get(customerId);
+
+            // Assert
+            mockRepository.VerifyAll();
+            Assert.NotNull(result);
+            Assert.Equal(100, result.TotalRewardPoints);
+            Assert.Single(result.MonthlyRewardPoints);
+        }
+    }
+}
---
+++ Customer.Rewards.Api/Customer.Rewards.Api.Tests/Managers/CustomerRewardsManagerTests.cs
@@ -0,0 +1,58 @@
+﻿using Customer.Rewards.Api.Exceptions;
+using Customer.Rewards.Api.Managers;
+using Customer.Rewards.Api.Models;
+using Customer.Rewards.Api.Repository.Interface;
+using Moq;
+using Xunit;
+
+namespace Customer.Rewards.Api.Tests.Managers
+{
+    public class CustomerRewardsManagerTests
+    {
+        private readonly MockRepository mockRepository;
+
+        private readonly Mock<ITransactionHistoryRepository> mockTransactionHistoryRepository;
+
+        public CustomerRewardsManagerTests()
+        {
+            mockRepository = new MockRepository(MockBehavior.Strict);
+            mockTransactionHistoryRepository = mockRepository.Create<ITransactionHistoryRepository>();
+        }
+
+        [Fact]
+        public void GetCustomerRewardsById_Returns_Rewards()
+        {
+            // Arrange
+            long customerId = 1;
+            mockTransactionHistoryRepository.Setup(c => c.GetTransactionHistory(customerId)).Returns(
+                new List<TransactionHistory>
+                {
+                    new TransactionHistory { Id = 1, Amount = 10, CustomerId = 1, TransactionDate = new DateTime(2023, 1, 1) },
+                    new TransactionHistory { Id = 3, Amount = 200, CustomerId = 1, TransactionDate = new DateTime(2023, 2, 2) },
+                }
+                );
+
+            var manager = new CustomerRewardsManager(mockTransactionHistoryRepository.Object);
+
+            // Act
+            var result = manager.GetCustomerRewardsById(
+                customerId);
+
+            // Assert
+            mockRepository.VerifyAll();
+            Assert.NotNull(result);
+            Assert.Equal(250, result.TotalRewardPoints);
+        }
+
+        [Fact]
+        public void GetCustomerRewardsById_Throw_Exception()
+        {
+            // Arrange
+            long customerId = -1;
+            var manager = new CustomerRewardsManager(mockTransactionHistoryRepository.Object);
+
+            // Act
+            Assert.Throws<InvalidCustomerIdException>(() => manager.GetCustomerRewardsById(customerId));
+        }
+    }
+}
